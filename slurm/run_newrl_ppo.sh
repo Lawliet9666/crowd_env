b5b03cbef1747dd32810d98591d34e76fdb6040c
Lawliet9666 <13181269398@163.com>
2026-03-08 16:17:45 -0400

    crowd sim param

--- slurm/run_newrl_ppo.sh
+++ slurm/run_newrl_ppo.sh
@@ -4,13 +4,17 @@ export WANDB_API_KEY=wandb_v1_ClN12layxh5kKSL1YPzMFMrSELm_KVPdDFXHeR4loGB8Ck8iUT
 # pip install -e .
 
 python scripts/run_crowdsim_ppo_base.py \
-  run_name=ppo_cvar_2nets \
+  run_name=newrl_ppo_cvar_2nets \
   wandb_entity=xinywa_umich \
-  wandb_project=crowdsim_ppo \
+  wandb_project=newrl_ppo \
   method=rlcvarbetaradius_2nets \
   trainer.total_steps=20000000 \
   trainer.batch_size=8192 \
   trainer.minibatch_size=256 \
   trainer.num_envs=32 \
-  
-# bash slurm/run.sh
+  env.topk=1 \
+  overwrite=true
+
+
+
+# bash slurm/run_newrl_ppo.sh
